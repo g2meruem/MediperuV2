fcaeb89db0c47ec568e2dc597fc2c7a92bb08f40
g2meruem <70904882+g2meruem@users.noreply.github.com>
2023-05-08 08:16:03 -0300

    bug

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styles: [`
-        :host ::ng-deep .pi-eye,
-        :host ::ng-deep .pi-eye-slash {
-            transform:scale(1.6);
-            margin-right: 1rem;
-            color: var(--primary-color) !important;
-        }
-    `]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [`
+    :host ::ng-deep .pi-eye,
+    :host ::ng-deep .pi-eye-slash {
+      transform: scale(1.6);
+      margin-right: 1rem;
+      color: var(--primary-color) !important;
+    }
+  `]
 })
 export class LoginComponent {
 
-    valCheck: string[] = ['remember'];
+  valCheck: string[] = ['remember'];
+  password: string = '';
+  email: string = '';
 
-    password!: string;
+  constructor(public layoutService: LayoutService, public afAuth: AngularFireAuth) {}
 
-    constructor(public layoutService: LayoutService) { }
+  async login() {
+    try {
+      const result = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
