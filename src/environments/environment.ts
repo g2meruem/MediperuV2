0705d431a964baf34ae8c97d33368074bfe5eae3
g2meruem <70904882+g2meruem@users.noreply.github.com>
2023-06-05 10:48:45 -0300

    Revert "bug"
    This reverts commit fcaeb89db0c47ec568e2dc597fc2c7a92bb08f40.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyB5QUE8PRV9zZqOKFlwoZKEzLBDk63nA6A",
-        authDomain: "sistema-medico-mediperu.firebaseapp.com",
-        projectId: "sistema-medico-mediperu",
-        storageBucket: "sistema-medico-mediperu.appspot.com",
-        messagingSenderId: "321355340785",
-        appId: "1:321355340785:web:bdc2e1c7a711ba448a4250",
-        measurementId: "G-EWR86B6J8F"
+      // Aquí van las variables de configuración de Firebase
     }
   };
